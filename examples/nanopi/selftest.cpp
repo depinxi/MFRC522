3906c6c2ee40292a1fd0d57eda7efc50f0086f04
Renaud Guillard <renaud@depinxi.be>
2018-02-02 16:37:48 +0100

    nanopi self test

--- examples/nanopi/selftest.cpp
+++ examples/nanopi/selftest.cpp
@@ -15,36 +15,9 @@ void delay(int ms)
 int main(int argc, const char **argv)
 {
 	MFRC522 mfrc;
-	
 	mfrc.PCD_Init();
-	
-	printf("Version : %02X\n", mfrc.PCD_ReadRegister(MFRC522::VersionReg));
-	
-	if (argc > 1 && atoi(argv[1])) {
-		int result =  mfrc.PCD_PerformSelfTest();
-		printf("test: %d\n", result);
-		return ((result == 1) ? 0 : 1);
-	}
-	
-	while (1)
-	{
-		if (mfrc.PICC_IsNewCardPresent())
-		{
-			if (mfrc.PICC_ReadCardSerial())
-			{
-				MFRC522::PICC_Type piccType = (MFRC522::PICC_Type)mfrc.PICC_GetType(mfrc.uid.sak);
-				
-				if (piccType == MFRC522::PICC_TYPE_MIFARE_1K)
-				{
-					printf("MIFARE 1K\n");
-				}
-			}
-			
-			mfrc.PICC_HaltA();
-			mfrc.PCD_StopCrypto1();
-		}
-	}
-	
-	return 0;
+	int version = mfrc.PCD_ReadRegister(MFRC522::VersionReg);
+	printf("Version : %02X\n", version); 
+	return (version ? 0 : 1);
 }
 
